d79dda2dfc57283698cd78edc4803c7ebb396ab0
beardybones <beardybonescodes@gmail.com>
2018-12-14 14:02:49 -0800

    changed home.html to index.html

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var waitlist = [
 
   // Basic route that sends the user first to the AJAX Page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
   });
   
   app.get("/tables", function(req, res) {
