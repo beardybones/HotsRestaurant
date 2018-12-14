3000b1a74e907132b0c7dbf15ffcc7a593c8631d
EKS <saadeh.emad@gmail.com>
2018-12-14 13:46:34 -0800

    Phase II

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -25,6 +25,44 @@ app.use(express.json());
   app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
   });
+
+  app.get("/api/:urlInput", function(req, res) {
+    var chosen = req.params.urlInput;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < reservations.length; i++) {
+      if (chosen === reservations[i].routeName) {
+        return res.json(reservations[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+
+  // Create New Characters - takes in JSON input
+  app.post("/api/reservations", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.name = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    newReservation.phone = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    newReservation.email = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    newReservation.uniqueID = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newReservation);
+  
+    reservations.push(newReservation);
+  
+    res.json(newReservation);
+  });
+
+
+
+
+
 
 
   app.listen(PORT, function() {
